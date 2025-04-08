d653d310403384fa3abe9cfd420dab894320d5ed
xaypcsharp <113992162+Xaypthox@users.noreply.github.com>
2025-04-09 01:42:44 +0300

    Add files via upload
    İlk C# projem: Not Ortalaması Hesaplayıcı

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("1. not: ");
+        int not1 = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("2. not: ");
+        int not2 = Convert.ToInt32(Console.ReadLine());
+
+        double ort = (not1 + not2) / 2.0;
+        Console.WriteLine("Ortalama: " + ort);
+    }
+}
